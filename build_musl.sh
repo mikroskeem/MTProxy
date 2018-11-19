b5fb0d93d9c7aac6c1ee244788791d2214cc56aa
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2018-11-20 01:33:36 +0200

    Use libz instead of zlib

--- build_musl.sh
+++ build_musl.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 LIBRESSL_URL="https://ftp.openbsd.org/pub/OpenBSD/LibreSSL/libressl-2.8.2.tar.gz"
-ZLIB_URL="https://zlib.net/zlib-1.2.11.tar.gz"
+LIBZ_URL="https://sortix.org/libz/release/libz-1.2.8.2015.12.26.tar.gz"
 LIBEXECINFO_URL="https://github.com/mikroskeem/libexecinfo/archive/1.1-2.tar.gz"
 
 BUILDROOT="$(pwd)/musl_buildroot"
 LIBRESSL_SRC="${BUILDROOT}/libressl-src.tar.gz"
-ZLIB_SRC="${BUILDROOT}/zlib-src.tar.gz"
+LIBZ_SRC="${BUILDROOT}/libz-src.tar.gz"
 LIBEXECINFO_SRC="${BUILDROOT}/libexecinfo-src.tar.gz"
 
 export CC="musl-gcc"
@@ -18,7 +18,7 @@ if [ ! -d "${BUILDROOT}" ] || [ "${1}" = "buildroot" ]; then
 
     # Download
     [ -f "${LIBRESSL_SRC}" ] || curl -o "${LIBRESSL_SRC}" "${LIBRESSL_URL}"
-    [ -f "${ZLIB_SRC}" ] || curl -o "${ZLIB_SRC}" "${ZLIB_URL}"
+    [ -f "${LIBZ_SRC}" ] || curl -o "${LIBZ_SRC}" "${LIBZ_URL}"
     [ -f "${LIBEXECINFO_SRC}" ] || curl -L -o "${LIBEXECINFO_SRC}" "${LIBEXECINFO_URL}"
 
     # Build LibreSSL
@@ -32,10 +32,10 @@ if [ ! -d "${BUILDROOT}" ] || [ "${1}" = "buildroot" ]; then
     make -j"$(grep -c ^processor /proc/cpuinfo)"
     make install
 
-    # Build zlib
+    # Build libz
     cd "${BUILDROOT}" || exit 1
-    tar xvf "${ZLIB_SRC}"
-    cd zlib-*/ || exit 1
+    tar xvf "${LIBZ_SRC}"
+    cd libz-*/ || exit 1
 
     mkdir build && cd build || exit 1;
     ../configure --prefix="${BUILDROOT}"
@@ -53,7 +53,7 @@ if [ ! -d "${BUILDROOT}" ] || [ "${1}" = "buildroot" ]; then
     # Cleanup
     cd "${BUILDROOT}" || exit 1
     rm -rf libressl-*/
-    rm -rf zlib-*/
+    rm -rf libz-*/
     rm -rf libexecinfo-*/
 
     cd "${BUILDROOT}/.." || exit 1
